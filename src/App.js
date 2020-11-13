0bb5ae1e72d480ded83a762c0527d6d88efe9c15
woojerry <50645183+woojerry@users.noreply.github.com>
2020-11-13 18:46:55 +0900

    Component 및 기본틀 완료

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './TodolistTemplate.js';
 import TodoHead from './TodoHead.js';
 import TodoList from './TodoList.js';
+import TodoCreate from './TodoCreate.js';
 
 const GlobalStyle = createGlobalStyle` // 전역에 styled component 사용
   body {
@@ -18,8 +19,13 @@ function App() {
     <>
     <GlobalStyle />
       <TodoTemplate>
+
         <TodoHead />
+        
         <TodoList />
+        
+        <TodoCreate />
+
       </TodoTemplate>
     </>
   );
